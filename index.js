469590d0520472eef2359ed3d64d5d5aba57218c
Roberto Rossa <rcrossa@hotmail.com>
2022-11-30 21:56:08 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+let product = require('./product');
+const app = express()
+const port = 8080
+
+
+app.get('/api', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`La app esta escuchando en el puerto ${port}`)
+});
+
+app.get('/api/products', product.list);
+app.get('/api/productsRandom', product.list1);
---
+++ product.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+exports.list = function (req, res, next) {
+    fs.readFile('./products.json', 'utf8', (error, contenido) => {
+        if (error) {
+            console.log(error)
+        } else {
+            let datos = JSON.parse(contenido);
+            res.send(datos);
+        }
+    })
+
+}
+
+exports.list1 = function (req, res, next) {
+    fs.readFile('./products.json', 'utf8', (error, contenido) => {
+        if (error) {
+            console.log(error)
+        } else {
+            let datos = JSON.parse(contenido);
+            let rand = Math.floor(Math.random() * datos.length)
+            let rValue = datos[rand];
+            res.send(rValue);
+        }
+    })
+
+}
